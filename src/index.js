1fbc9499b835fd4afab6fe54b4924a3dc3aca79d
FerAlvarez11 <99476885+FerAlvarez11@users.noreply.github.com>
2023-03-24 17:24:40 +0000

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(    
-    <HashRouter basename="PlantCard">
+root.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
-    </HashRouter>        
+    </BrowserRouter>
 );
 
