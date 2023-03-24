5904bbe044b591b86b4e7ba6e30de630763bd270
FerAlvarez11 <99476885+FerAlvarez11@users.noreply.github.com>
2023-03-24 17:31:48 +0000

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import PlantList from "./Components/PlantList";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AddPlant from "./Components/AddPlant";
 import Homepage from "./Components/Homepage";
 import "./App.css";
@@ -8,11 +8,13 @@ import "bulma/css/bulma.min.css"
 function App() {    
     return (    
         <div className="App">   
-            <Routes>
-                <Route path="/" element={<Homepage/>}/>
-                <Route path="/add-plant" element={<AddPlant/>}/>
-                <Route path="/plant-list" element={<PlantList/>}/>
-            </Routes>    
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
+                <Routes>
+                    <Route path="/" element={<Homepage/>}/>
+                    <Route path="/add-plant" element={<AddPlant/>}/>
+                    <Route path="/plant-list" element={<PlantList/>}/>
+                </Routes> 
+            </BrowserRouter>   
         </div>
     );
 }
